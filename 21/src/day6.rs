9ed0e9f76aef1f10ab26c0f16a050d2744c119e9
Max Weber <mii7303@gmail.com>
2021-12-05 23:36:45 -0700

    smarter 2021d6

--- 21/src/day6.rs
+++ 21/src/day6.rs
@@ -7,19 +7,12 @@ fn day6_part2(input: &str) -> usize {
 	solve(input, 256)
 }
 fn solve(input: &str, days: usize) -> usize {
-	let mut state: Vec<usize> = vec![0; 9];
-	let mut next_state = state.clone();
-	for fish in input.trim().split(",").map(|x| x.parse::<usize>().unwrap()) {
-		state[fish] += 1;
+	let mut state = [0; 9];
+	for fish in input.trim().split(",") {
+		state[fish.parse::<usize>().unwrap()] += 1;
 	}
-	for _day in 0..days {
-		next_state.fill(0);
-		for lifespan in 1..=8 {
-			next_state[lifespan - 1] += state[lifespan];
-		}
-		next_state[6] += state[0];
-		next_state[8] += state[0];
-		std::mem::swap(&mut state, &mut next_state);
+	for day in 0..days {
+		state[(day + 7) % state.len()] += state[day % state.len()];
 	}
 	state.iter().sum()
 }
