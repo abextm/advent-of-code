fca5a9e9a0d64981377f5ae63da3822a9bf1ec1d
Max Weber <mii7303@gmail.com>
2022-12-01 22:21:04 -0700

    2022d2

---
+++ 22/src/day2.rs
@@ -0,0 +1,32 @@
+#[aoc(day2, part1)]
+fn day2_part1(input: &str) -> usize {
+	input.trim().split("\n").map(|line| {
+		let opponent = line.bytes().nth(0).unwrap() - b'A';
+		let me = line.bytes().nth(2).unwrap() - b'X';
+
+		let score = me + 1;
+
+		let win = if me == opponent {
+			3
+		} else if me == (opponent + 1) % 3 {
+			6
+		} else {
+			0
+		};
+
+		return (win + score) as usize;
+	}).sum()
+}
+
+#[aoc(day2, part2)]
+fn day2_part2(input: &str) -> usize {
+	input.trim().split("\n").map(|line| {
+		let opponent = line.bytes().nth(0).unwrap() - b'A';
+		let outcome = line.bytes().nth(2).unwrap() - b'X';
+
+		let win = (outcome * 3) as usize;
+		let me = (outcome + 2 + opponent) % 3;
+
+		return win + (me + 1) as usize
+	}).sum()
+}
