0acdd56239a8c9609190af6f4b07074bbc3f2327
Max Weber <mii7303@gmail.com>
2023-12-22 02:38:39 -0700

    2023d22 opt

--- 23/src/day22.rs
+++ 23/src/day22.rs
@@ -1,3 +1,5 @@
+use smallvec::SmallVec;
+
 type Point = [isize; 3];
 
 #[derive(Debug)]
@@ -5,75 +7,64 @@ struct Brick {
 	low: Point,
 	hi: Point,
 
-	below: Vec<usize>,
-	above: Vec<usize>,
+	below_count: usize,
+	above: SmallVec<[u16; 4]>,
 }
 
 fn parse_bricks(input: &str) -> Vec<Brick> {
-	//let mut max_height = 0;
+	let mut xy_max = [0isize; 2];
 	let mut bricks = input.lines()
 		.map(|line| {
-			let coords = line.split("~")
+			let [low, hi] = line.split("~")
 				.map(|coord| coord.split(",").map(|c| c.parse::<isize>().expect(c)).array_chunks::<3>().next().unwrap())
 				.array_chunks::<2>().next().unwrap();
-			//max_height = max_height.max(coords[1][2] - coords[0][2]);
+			xy_max[0] = xy_max[0].max(hi[0]);
+			xy_max[1] = xy_max[1].max(hi[1]);
 			Brick {
-				low: coords[0],
-				hi: coords[1],
+				low,
+				hi,
 
-				below: Vec::new(),
-				above: Vec::new(),
+				below_count: 0,
+				above: SmallVec::new(),
 			}
 		})
 		.collect::<Vec<_>>();
 
-	bricks.sort_by_key(|b| b.low[2]);
+	bricks.sort_unstable_by_key(|b| b.low[2]);
 
-	let mut touching_bricks = Vec::<usize>::new();
+	let grid_width = xy_max[0] + 1;
+	let mut grid = vec![(0, u16::MAX); (grid_width * (xy_max[1] + 1)) as usize];
+
+	let mut touching_bricks = Vec::<u16>::new();
 
 	for brick_id in 0..bricks.len() {
-		let brick = &bricks[brick_id];
-
 		touching_bricks.clear();
-		let mut max_hi = 0;
+		let Brick{low, hi, ..} = bricks[brick_id];
 
-		for inner_brick_id in (0..brick_id).rev() {
-			let ibrick = &bricks[inner_brick_id];
-			if ibrick.hi[2] < max_hi || ibrick.low[2] > brick.low[2] {
-				continue;
-			}
-			let outside = brick.low[0] > ibrick.hi[0] || brick.hi[0] < ibrick.low[0]
-				|| brick.low[1] > ibrick.hi[1] || brick.hi[1] < ibrick.low[1];
-			if outside {
-				continue;
-			}
-
-			if ibrick.low[2] == brick.low[2] {
-				panic!("{:?} {:?}", brick, ibrick)
-			}
-
-			if ibrick.hi[2] > max_hi {
-				max_hi = ibrick.hi[2];
-				touching_bricks.clear();
-			}
-			if ibrick.hi[2] == max_hi {
-				touching_bricks.push(inner_brick_id);
-			}
-		}
-
-		for &inner_brick_id in touching_bricks.iter() {
-			bricks[inner_brick_id].above.push(brick_id);
-		}
-
+		let brick_iter = (low[1]..=hi[1])
+			.flat_map(|y| (low[0]..=hi[0]).map(move |x| (x + y * grid_width) as usize));
+		
+		let max_hi = brick_iter.clone().map(|pt| grid[pt].0).max().unwrap();
+		
 		let brick = &mut bricks[brick_id];
 		let delta = brick.low[2] - (max_hi + 1);
 		brick.low[2] -= delta;
 		brick.hi[2] -= delta;
+		let top = brick.hi[2];
 
-		assert_eq!(brick.above.len(), 0);
-		assert_eq!(brick.below.len(), 0);
+		for pt in brick_iter {
+			let (ihi, ibrick_id) = grid[pt];
+			if max_hi == ihi && ibrick_id != u16::MAX && !touching_bricks.contains(&ibrick_id) {
+				touching_bricks.push(ibrick_id);
+			}
+			grid[pt] = (top, brick_id as u16);
+		}
 
-		std::mem::swap(&mut touching_bricks, &mut brick.below);
+		brick.below_count = touching_bricks.len();
+
+		for &inner_brick_id in touching_bricks.iter() {
+			bricks[inner_brick_id as usize].above.push(brick_id as u16);
+		}
 	}
 
 	bricks
@@ -83,27 +74,22 @@ fn parse_bricks(input: &str) -> Vec<Brick> {
 fn part1(input: &str) -> usize {
 	let bricks = parse_bricks(input);
 	bricks.iter().filter(|br| {
-		br.above.iter().all(|&bid| bricks[bid].below.len() > 1)
+		br.above.iter().all(|&bid| bricks[bid as usize].below_count > 1)
 	}).count()
 }
 
-fn count_fallen(bricks: &[Brick], fallen: &mut [bool], index: usize) -> usize {
-	if fallen[index] {
-		return 0;
-	}
-	let brick = &bricks[index];
-	if brick.below.iter().all(|&ib| fallen[ib]) {
-		fall_brick(bricks, fallen, index)
-	} else {
-		0
-	}
-}
-
-fn fall_brick(bricks: &[Brick], fallen: &mut [bool], index: usize) -> usize {
-	fallen[index] = true;
-	let mut sum = 1;
-	for &b in bricks[index].above.iter() {
-		sum += count_fallen(bricks, fallen, b);
+fn fall_brick(bricks: &[Brick], supports: &mut [u8], todo: &mut Vec<usize>, index: usize) -> usize {
+	let mut sum = 0;
+	todo.push(index);
+	while let Some(index) = todo.pop() {
+		for &b in bricks[index].above.iter() {	
+			let supp = supports[b as usize] - 1;
+			supports[b as usize] = supp;
+			if supp == 0 {
+				sum += 1;
+				todo.push(b as usize);
+			}
+		}
 	}
 
 	sum
@@ -112,10 +98,17 @@ fn fall_brick(bricks: &[Brick], fallen: &mut [bool], index: usize) -> usize {
 #[aoc(day22, part2)]
 fn part2(input: &str) -> usize {
 	let bricks = parse_bricks(input);
-	let mut fallen = vec![false; bricks.len()];
+	let supports_template = bricks.iter()
+		.map(|b| b.below_count as u8)
+		.collect::<Vec<_>>();
+	let mut supports = supports_template.clone();
+	let mut todo = Vec::new();
 	(0..bricks.len()).map(|id| {
-		fallen.fill(false);
-		fall_brick(&bricks, &mut fallen, id) - 1
+		if id != 0 {
+			supports.copy_from_slice(&supports_template);
+		}
+		let v = fall_brick(&bricks, &mut supports, &mut todo, id);
+		v
 	}).sum()
 }
 
