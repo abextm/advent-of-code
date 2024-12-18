897f42b8a8d7396d2606297f5a89500707a769be
Max Weber <mii7303@gmail.com>
2024-12-18 01:03:30 -0700

    2024d18 alt

--- 24/src/day18.rs
+++ 24/src/day18.rs
@@ -1,4 +1,4 @@
-use crate::dijkstra::dijkstra;
+use crate::dijkstra::{dijkstra, State};
 use crate::grid::{adj4, Grid, Ve};
 
 #[aoc(part1=308)]
@@ -7,13 +7,69 @@ fn part1(input: &str) -> impl std::fmt::Debug {
 }
 
 #[aoc(part2="\"46,28\"")]
-fn part2(input: &str) -> impl std::fmt::Debug {
+fn part2_2(input: &str) -> impl std::fmt::Debug {
+	let mut g = Grid::new([71, 71], u16::MAX);
+	for (i, line) in input.trim().lines().enumerate() {
+		let pt = Ve::<2>(line.split(",").map(|n| n.parse().unwrap()).next_chunk().unwrap());
+		g[pt] = i as u16;
+	}
+
+	let mut dist = Grid::new(g.shape(), false);
+	let goal_tester = |&pt: &Ve<2>| {pt[0] == 0 || pt[1] == 70};
+
+	let mut heap = std::collections::BinaryHeap::new();
+	for start in (1..=70).map(|x| Ve([x, 0])).chain((1..70).map(|y| Ve([70, y]))) {
+		let v = g[start];
+		if v != u16::MAX {
+			let cost = v as usize;
+			heap.push(State {
+				cost,
+				index: start,
+			});
+		}
+	}
+
+	while let Some(state) = heap.pop() {
+		if goal_tester(&state.index) {
+			return input.lines().nth(state.cost).unwrap().to_owned();
+		}
+
+		if dist[state.index] {
+			continue;
+		}
+		dist[state.index] = true;
+
+		let pt = state.index;
+		let g = &g;
+		for (index, cost) in adj4().filter_map(move |d| {
+			if let Some(&v) = g.get(pt + d) {
+				if v != u16::MAX {
+					let v = v as usize;
+					return Some((pt + d, state.cost.max(v)))
+				}
+			}
+			return None
+		}) {
+			if !dist[index] {
+				heap.push(State {
+					index: index.clone(),
+					cost,
+				});
+			}
+		}
+	}
+
+	panic!()
+}
+
+#[allow(unused)]
+fn part2_1(input: &str) -> impl std::fmt::Debug {
 	let n = input.lines().count();
 	let mut low = 0;
 	let mut high = n - 1;
 	while low <= high {
 		let mid = (low + high) / 2;
-		if let Some(v) = solve(input, mid + 1) {
+		if let Some(_) = solve(input, mid + 1) {
 			low = mid + 1;
 		} else {
 			high = mid - 1;
